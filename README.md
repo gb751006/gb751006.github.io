229cbd0e90f8ffdd9f1e1baffecf0be39d97c841
gb751006 <115118043+gb751006@users.noreply.github.com>
2023-03-16 05:24:10 +0530

    protfolio

---
+++ README.md
@@ -0,0 +1,24 @@
+# Personal Master Portfolio Website 👨‍💻
+Hello there, welcome to the repository of my Personal Portfolio!! <br>
+This is still a work in progress <br>
+If you find any bugs you can report it to me.
+
+![Capture](https://user-images.githubusercontent.com/99565432/171957070-2cb3c504-97f5-49c8-b43a-c9008e5c3336.PNG)
+![Capture2](https://user-images.githubusercontent.com/99565432/171957646-6900d101-6cc1-4368-b0b3-8d967e798fd5.PNG)
+![Capture3](https://user-images.githubusercontent.com/99565432/171957677-e42f0853-1aad-4115-b4c4-6383278b938e.PNG)
+![Capture4](https://user-images.githubusercontent.com/99565432/171957709-35ce9098-95cd-4ebc-bee7-cfa4629bae66.PNG)
+![Capture5](https://user-images.githubusercontent.com/99565432/171957733-29184a83-99ca-433a-b0e3-67ec401a5d64.PNG)
+
+
+
+## Tools/Technologies Used
+
+1) React JS
+2) React Scroll
+3) React Reveal
+4) React Helmet
+5) Styled Components
+6) Email JS
+
+
+https://ganesh-portfolio-react.vercel.app/
---
+++ src/components/navbar.js
@@ -0,0 +1,87 @@
+import "../App.css";
+import { useState } from "react";
+function Navbar() {
+  const [toggle, settoggle] = useState(false);
+  const [small, setsmall] = useState("");
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img
+          src="https://pngpress.com/wp-content/uploads/2020/09/uploads_letter_g_letter_g_PNG59.png"
+          width="50px"
+          height="50px"
+          alt=""
+          style={{ borderRadius: "50%" }}
+        />
+      </div>
+      <div
+        className="menu_toggle"
+        onClick={() => {
+          settoggle(!toggle);
+          setsmall("small");
+        }}
+      >
+        <img
+          src="https://cdn2.iconfinder.com/data/icons/flat-and-simple-part-2/128/menu-512.png"
+          width="50px"
+          height="50px"
+          alt=""
+        />
+      </div>
+      <div
+        className={
+          toggle && small === "small" ? "small_nav right--nav" : "right--nav"
+        }
+      >
+        <div
+          className="active"
+          onClick={() => {
+            settoggle(!toggle);
+          }}
+        >
+          <a href="#top">Home</a>
+        </div>
+        <div
+          onClick={() => {
+            settoggle(!toggle);
+          }}
+        >
+          <a href="#about">About</a>
+        </div>
+        <div
+          onClick={() => {
+            settoggle(!toggle);
+          }}
+        >
+          <a href="#tech">Skills</a>
+        </div>
+        <div
+          onClick={() => {
+            settoggle(!toggle);
+          }}
+        >
+          <a href="#projects">Project</a>
+        </div>
+        <div
+          onClick={() => {
+            settoggle(!toggle);
+          }}
+        >
+          <a href="#contact">Contact Me</a>
+        </div>
+        <div className="cv">
+          <a
+            href="https://drive.google.com/file/d/1eqa_H_1lpNAyD8WjEhq8qy8NxcnGklWT/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#000" }}
+          >
+            Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
